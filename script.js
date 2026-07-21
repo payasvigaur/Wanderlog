62e1a153aaf3a9c00148271ecbabb1e2c0beb67f
Payasvi <payasvigaur@gmail.com>
2026-07-21 21:23:07 +0530

    Week 3 progress - image upload and trip management

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 const tripForm = document.getElementById("tripForm");
 const tripList = document.getElementById("tripList");
 
+const imageInput = document.getElementById("image");
+const preview = document.getElementById("preview");
+
+let imageData = "";
+let trips= JSON.parse(localStorage.getItem("trips"))||[];
+
+imageInput.addEventListener("change", function(){
+
+    const file = this.files[0];
+
+    if(file){
+
+        const reader = new FileReader();
+
+        reader.onload = function(e){
+
+            imageData = e.target.result;
+
+            preview.src = imageData;
+
+            preview.style.display = "block";
+
+        };
+
+        reader.readAsDataURL(file);
+
+    }
+
+});
+
 tripForm.addEventListener("submit", function(e) {
 
     e.preventDefault();
@@ -15,6 +45,7 @@ tripForm.addEventListener("submit", function(e) {
     card.className = "trip-card";
 
     card.innerHTML = `
+    <img src="${imageData}" class="trip-image">
         <h3>🌸 ${title}</h3>
         <p><strong>Destination:</strong> ${destination}</p>
         <p><strong>Date:</strong> ${date}</p>
@@ -24,6 +55,15 @@ tripForm.addEventListener("submit", function(e) {
     `;
 
     tripList.appendChild(card);
+    trips.push({
+    title,
+    destination,
+    date,
+    notes,
+    image: imageData
+});
+
+localStorage.setItem("trips", JSON.stringify(trips));
 
     tripForm.reset();
 
@@ -41,3 +81,30 @@ document.addEventListener("click", function(e){
     }
 
 });
+window.onload = function(){
+
+    trips.forEach(function(trip){
+
+        const card = document.createElement("div");
+
+        card.className = "trip-card";
+
+        card.innerHTML = `
+            <img src="${trip.image}" class="trip-image">
+
+            <h3>🌸 ${trip.title}</h3>
+
+            <p><strong>Destination:</strong> ${trip.destination}</p>
+
+            <p><strong>Date:</strong> ${trip.date}</p>
+
+            <p>${trip.notes}</p>
+
+            <button class="deleteBtn">Delete</button>
+        `;
+
+        tripList.appendChild(card);
+
+    });
+
+};
